22e76eef9525f8fb0e533858d905208f062998e7
wangkai <wangkka1@163.com>
2021-12-04 21:52:02 +0800

    init project

--- node-ts-temp/templates/node-ts-boilerplate/config/base.config.js
+++ node-ts-temp/templates/node-ts-boilerplate/config/base.config.js
@@ -1,48 +1,48 @@
-const esbuild = require('esbuild');
-const chalk = require('chalk');
-const { nodeExternalsPlugin } = require('esbuild-node-externals');
+const esbuild = require("esbuild");
+const chalk = require("chalk");
+const { nodeExternalsPlugin } = require("esbuild-node-externals");
 
 const startPlugin = {
-  name: 'start',
+  name: "start",
   setup(build) {
     build.onStart(() => {
-      console.log('compiling typescript files...');
+      console.log("compiling typescript files...");
     });
   },
 };
 
 module.exports = function (env) {
-  const isDevelopment = env === 'development';
-  const isProduction = env === 'production';
+  const isDevelopment = env === "development";
+  const isProduction = env === "production";
 
   return esbuild
     .build({
-      entryPoints: ['scripts/index.ts'],
+      entryPoints: ["scripts/index.ts"],
       bundle: true,
       minify: true,
       define: {
-        'process.env.NODE_ENV': JSON.stringify(env),
+        "process.env.NODE_ENV": JSON.stringify(env),
       },
-      platform: 'node',
-      target: ['es2020', 'node12'],
+      platform: "node",
+      target: ["node12"],
       loader: {
-        '.ts': 'ts',
+        ".ts": "ts",
       },
-      tsconfig: 'tsconfig.json',
+      tsconfig: "tsconfig.json",
       plugins: [startPlugin, nodeExternalsPlugin()],
-      outfile: 'build/index.js',
+      outfile: "build/index.js",
       watch: isDevelopment && {
         onRebuild(error, result) {
           if (error) {
-            console.error(chalk.red('compiled failed:'), error);
+            console.error(chalk.red("compiled failed:"), error);
           } else {
-            console.log(chalk.green('compiled success'));
+            console.log(chalk.green("compiled success"));
           }
         },
       }, // make esbuild listen tsfile change to rebuild automaticly
     })
     .then(() => {
-      console.log(chalk.green('compiled success'));
+      console.log(chalk.green("compiled success"));
     })
     .catch(() => process.exit(1));
 };
